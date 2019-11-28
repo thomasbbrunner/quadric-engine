872eee98742c472be6a291e58408e0b5ab31834c
Thomas Brunner <thomasbbrunner@gmail.com>
2019-11-28 20:17:19 +0100

    Added tests for string parsing and modification

---
+++ tests/string_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main()
+{
+    int test_int = 998;
+    std::string test_str = "The int is: " + "asdf"; //<< test_int;
+
+    std::cout << std::endl;
+
+    return 0;
+}
