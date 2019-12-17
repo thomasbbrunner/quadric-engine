c23390bf1427e01ab9f20ec83435b1f296fe60f6
Thomas Brunner <thomasbbrunner@gmail.com>
2019-12-17 01:18:50 +0100

    Renamed enum class TimeType to Type in Tiktok class

--- inc/tiktok.hpp
+++ inc/tiktok.hpp
@@ -25,7 +25,7 @@ private:
     // End of Singleton initialization
 
 public:
-    enum class TimeType
+    enum class Type
     {
         REAL,
         TICKS,
@@ -35,21 +35,21 @@ private:
     double tiktok_wall_ = 0.0;
     double tiktok_tick_ = 0.0;
     double tiktok_tick_step_ = 1.0 / 60.0;
-    enum TimeType time_type_ = TimeType::TICKS;
+    enum Type time_type_ = Type::TICKS;
 
 public:
-    void set_type(enum TimeType type)
+    void set_type(enum Type type)
     {
         time_type_ = type;
     }
 
     double get()
     {
-        if (time_type_ == TimeType::TICKS)
+        if (time_type_ == Type::TICKS)
         {
             return tiktok_tick_;
         }
-        else if (time_type_ == TimeType::TICKS)
+        else if (time_type_ == Type::TICKS)
         {
             return tiktok_wall_;
         }
